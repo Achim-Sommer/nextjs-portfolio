1a61c6f857d2059bcdc9288fef985cdfcf423eeb
Achim <xgermangamingx@outlook.de>
2024-11-24 18:54:39 +0100

    feat: Re-enable PWA and enhance GitHub component

--- next.config.js
+++ next.config.js
@@ -5,126 +5,125 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
-// Temporarily disable PWA
-// const withPWA = require('next-pwa')({
-//   dest: 'public',
-//   disable: process.env.NODE_ENV === 'development',
-//   register: true,
-//   skipWaiting: true,
-//   runtimeCaching: [
-//     {
-//       urlPattern: /^https:\/\/fonts\.(?:gstatic)\.com\/.*/i,
-//       handler: 'CacheFirst',
-//       options: {
-//         cacheName: 'google-fonts-webfonts',
-//         expiration: {
-//           maxEntries: 4,
-//           maxAgeSeconds: 365 * 24 * 60 * 60
-//         }
-//       }
-//     },
-//     {
-//       urlPattern: /^https:\/\/fonts\.(?:googleapis)\.com\/.*/i,
-//       handler: 'StaleWhileRevalidate',
-//       options: {
-//         cacheName: 'google-fonts-stylesheets',
-//         expiration: {
-//           maxEntries: 4,
-//           maxAgeSeconds: 7 * 24 * 60 * 60
-//         }
-//       }
-//     },
-//     {
-//       urlPattern: /\.(?:eot|otf|ttc|ttf|woff|woff2|font.css)$/i,
-//       handler: 'StaleWhileRevalidate',
-//       options: {
-//         cacheName: 'static-font-assets',
-//         expiration: {
-//           maxEntries: 4,
-//           maxAgeSeconds: 7 * 24 * 60 * 60
-//         }
-//       }
-//     },
-//     {
-//       urlPattern: /\.(?:jpg|jpeg|gif|png|svg|ico|webp)$/i,
-//       handler: 'StaleWhileRevalidate',
-//       options: {
-//         cacheName: 'static-image-assets',
-//         expiration: {
-//           maxEntries: 64,
-//           maxAgeSeconds: 24 * 60 * 60
-//         }
-//       }
-//     },
-//     {
-//       urlPattern: /\/_next\/image\?url=.+$/i,
-//       handler: 'StaleWhileRevalidate',
-//       options: {
-//         cacheName: 'next-image',
-//         expiration: {
-//           maxEntries: 64,
-//           maxAgeSeconds: 24 * 60 * 60
-//         }
-//       }
-//     },
-//     {
-//       urlPattern: /\.(?:mp3|wav|ogg)$/i,
-//       handler: 'CacheFirst',
-//       options: {
-//         rangeRequests: true,
-//         cacheName: 'static-audio-assets',
-//         expiration: {
-//           maxEntries: 32,
-//           maxAgeSeconds: 24 * 60 * 60
-//         }
-//       }
-//     },
-//     {
-//       urlPattern: /\.(?:js)$/i,
-//       handler: 'StaleWhileRevalidate',
-//       options: {
-//         cacheName: 'static-js-assets',
-//         expiration: {
-//           maxEntries: 32,
-//           maxAgeSeconds: 24 * 60 * 60
-//         }
-//       }
-//     },
-//     {
-//       urlPattern: /\.(?:css|less)$/i,
-//       handler: 'StaleWhileRevalidate',
-//       options: {
-//         cacheName: 'static-style-assets',
-//         expiration: {
-//           maxEntries: 32,
-//           maxAgeSeconds: 24 * 60 * 60
-//         }
-//       }
-//     },
-//     {
-//       urlPattern: /\/_next\/data\/.+\/.+\.json$/i,
-//       handler: 'StaleWhileRevalidate',
-//       options: {
-//         cacheName: 'next-data',
-//         expiration: {
-//           maxEntries: 32,
-//           maxAgeSeconds: 24 * 60 * 60
-//         }
-//       }
-//     },
-//     {
-//       urlPattern: /\.(?:json|xml|csv)$/i,
-//       handler: 'StaleWhileRevalidate',
-//       options: {
-//         cacheName: 'static-data-assets',
-//         expiration: {
-//           maxEntries: 32,
-//           maxAgeSeconds: 24 * 60 * 60
-//         }
-//       }
-//     }
-//   ]
-// });
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+  runtimeCaching: [
+    {
+      urlPattern: /^https:\/\/fonts\.(?:gstatic)\.com\/.*/i,
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'google-fonts-webfonts',
+        expiration: {
+          maxEntries: 4,
+          maxAgeSeconds: 365 * 24 * 60 * 60
+        }
+      }
+    },
+    {
+      urlPattern: /^https:\/\/fonts\.(?:googleapis)\.com\/.*/i,
+      handler: 'StaleWhileRevalidate',
+      options: {
+        cacheName: 'google-fonts-stylesheets',
+        expiration: {
+          maxEntries: 4,
+          maxAgeSeconds: 7 * 24 * 60 * 60
+        }
+      }
+    },
+    {
+      urlPattern: /\.(?:eot|otf|ttc|ttf|woff|woff2|font.css)$/i,
+      handler: 'StaleWhileRevalidate',
+      options: {
+        cacheName: 'static-font-assets',
+        expiration: {
+          maxEntries: 4,
+          maxAgeSeconds: 7 * 24 * 60 * 60
+        }
+      }
+    },
+    {
+      urlPattern: /\.(?:jpg|jpeg|gif|png|svg|ico|webp)$/i,
+      handler: 'StaleWhileRevalidate',
+      options: {
+        cacheName: 'static-image-assets',
+        expiration: {
+          maxEntries: 64,
+          maxAgeSeconds: 24 * 60 * 60
+        }
+      }
+    },
+    {
+      urlPattern: /\/_next\/image\?url=.+$/i,
+      handler: 'StaleWhileRevalidate',
+      options: {
+        cacheName: 'next-image',
+        expiration: {
+          maxEntries: 64,
+          maxAgeSeconds: 24 * 60 * 60
+        }
+      }
+    },
+    {
+      urlPattern: /\.(?:mp3|wav|ogg)$/i,
+      handler: 'CacheFirst',
+      options: {
+        rangeRequests: true,
+        cacheName: 'static-audio-assets',
+        expiration: {
+          maxEntries: 32,
+          maxAgeSeconds: 24 * 60 * 60
+        }
+      }
+    },
+    {
+      urlPattern: /\.(?:js)$/i,
+      handler: 'StaleWhileRevalidate',
+      options: {
+        cacheName: 'static-js-assets',
+        expiration: {
+          maxEntries: 32,
+          maxAgeSeconds: 24 * 60 * 60
+        }
+      }
+    },
+    {
+      urlPattern: /\.(?:css|less)$/i,
+      handler: 'StaleWhileRevalidate',
+      options: {
+        cacheName: 'static-style-assets',
+        expiration: {
+          maxEntries: 32,
+          maxAgeSeconds: 24 * 60 * 60
+        }
+      }
+    },
+    {
+      urlPattern: /\/_next\/data\/.+\/.+\.json$/i,
+      handler: 'StaleWhileRevalidate',
+      options: {
+        cacheName: 'next-data',
+        expiration: {
+          maxEntries: 32,
+          maxAgeSeconds: 24 * 60 * 60
+        }
+      }
+    },
+    {
+      urlPattern: /\.(?:json|xml|csv)$/i,
+      handler: 'StaleWhileRevalidate',
+      options: {
+        cacheName: 'static-data-assets',
+        expiration: {
+          maxEntries: 32,
+          maxAgeSeconds: 24 * 60 * 60
+        }
+      }
+    }
+  ]
+});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -286,4 +285,4 @@ const nextConfig = {
   },
 };
 
-module.exports = withBundleAnalyzer(nextConfig);
+module.exports = withBundleAnalyzer(withPWA(nextConfig));
