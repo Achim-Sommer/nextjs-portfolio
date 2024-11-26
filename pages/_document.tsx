ca4676bf6fa2fac0ec2bd5ab20e0c6fa9e536e17
Achim <39227403+Achim-Sommer@users.noreply.github.com>
2024-11-26 19:57:01 +0100

    ühre ein NPM run build zum testen druch
    - Fixed canonical URL implementation for better SEO
    - Removed Font Awesome preload warning by updating link tag
    - Added app-build-manifest.json to PWA buildExcludes
    - Updated environment variables structure

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,18 +5,11 @@ export default function Document() {
     <Html lang="de">
       <Head>
         <link
-          rel="preload"
-          as="style"
+          rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
           integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
           crossOrigin="anonymous"
           referrerPolicy="no-referrer"
-          onLoad={() => {
-            const el = document.querySelector('link[rel="preload"][as="style"]');
-            if (el) {
-              el.setAttribute('rel', 'stylesheet');
-            }
-          }}
         />
         <noscript>
           <link
